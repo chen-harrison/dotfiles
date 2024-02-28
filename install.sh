f595dc8abde1973b1f3408b0ddaa3eb1145b0360
Harrison Chen <hchen.robotics@gmail.com>
2024-02-28 02:41:24 -0500

    New install.sh
    Create add_dotfile() function that replaces the previous repeated set of
    commands. Now, when a new file is added to the repo, a single line can
    be added to install.sh, run the script again, and it should be taken
    care of.

--- install.sh
+++ install.sh
@@ -1,49 +1,46 @@
 #!/usr/bin/env bash
 
+# Use this function to symlink dotfiles in the repo to their proper locations
+# \param $1 Absolute path to the dotfile you want to set a symlink for
+# \param $2 Absolute path to the symlink you want to set
+add_dotfile () {
+    local dotfile_path=${1}
+    local symlink_path=${2}
+    local file_path=''
+
+    echo "Symlinking ${dotfile_path} to ${symlink_path}"
+
+    # Get the file (or underlying file, for symlinks) at the symlink path
+    if [ -f ${symlink_path} ]; then
+        if [ -L ${symlink_path} ]; then
+            file_path=$(readlink -f ${symlink_path})
+        else
+            file_path=${symlink_path}
+        fi
+
+        # If the file is different from the source dotfile, we'll store it in prev_dotfiles
+        if [ "${dotfile_path}" != ${file_path} ]; then
+            echo "Saving ${file_path} to prev_dotfiles"
+            mv ${file_path} ${PWD}/prev_dotfiles/
+        fi
+    fi
+
+    ln -sf ${dotfile_path} ${symlink_path}
+}
+
+
 dotfiles_dir=$(dirname $0)
 cd ${dotfiles_dir}
 mkdir -p ${PWD}/prev_dotfiles
 
-# .bash_aliases
-[ -f ~/.bash_aliases ] && mv ~/.bash_aliases ${dotfiles_dir}/prev_dotfiles/
-ln -s ${PWD}/.bash_aliases ~/.bash_aliases
-
-# .bash_functions
-[ -f ~/.bash_functions ] && mv ~/.bash_functions ${dotfiles_dir}/prev_dotfiles/
-ln -s ${PWD}/.bash_functions ~/.bash_functions
-
-# .bashrc
-[ -f ~/.bashrc ] && mv ~/.bashrc ${dotfiles_dir}/prev_dotfiles/
-ln -s ${PWD}/.bashrc ~/.bashrc
-
-# .clang-format
-[ -f ~/.clang-format ] && mv ~/.clang-format ${dotfiles_dir}/prev_dotfiles/
-ln -s ${PWD}/.clang-format ~/.clang-format
-
-# .clang-tidy
-[ -f ~/.clang-tidy ] && mv ~/.clang-tidy ${dotfiles_dir}/prev_dotfiles/
-ln -s ${PWD}/.clang-tidy ~/.clang-tidy
-
-# .gitconfig
-[ -f ~/.gitconfig ] && mv ~/.gitconfig ${dotfiles_dir}/prev_dotfiles/
-ln -s ${PWD}/.gitconfig ~/.gitconfig
-
-# .gitignore_global
-[ -f ~/.gitignore_global ] && mv ~/.gitignore_global ${dotfiles_dir}/prev_dotfiles/
-ln -s ${PWD}/.gitignore_global ~/.gitignore_global
-
-# .gitmessage.txt
-[ -f ~/.gitmessage.txt ] && mv ~/.gitmessage.txt ${dotfiles_dir}/prev_dotfiles/
-ln -s ${PWD}/.gitmessage.txt ~/.gitmessage.txt
-
-# .nanorc
-[ -f ~/.nanorc ] && mv ~/.nanorc ${dotfiles_dir}/prev_dotfiles/
-ln -s ${PWD}/.nanorc ~/.nanorc
-
-# .tmux.conf
-[ -f ~/.tmux.conf ] && mv ~/.tmux.conf ${dotfiles_dir}/prev_dotfiles/
-ln -s ${PWD}/.tmux.conf ~/.tmux.conf
-
-# vscode_settings.json
-[ -f ~/.config/Code/User/settings.json ] && mv ~/.config/Code/User/settings.json ${dotfiles_dir}/prev_dotfiles/vscode_settings.json
-ln -s ${PWD}/vscode_settings.json ~/.config/Code/User/settings.json
+add_dotfile ~/dotfiles/.bash_aliases ~/.bash_aliases
+add_dotfile ~/dotfiles/.bash_functions ~/.bash_functions
+add_dotfile ~/dotfiles/.bashrc ~/.bashrc
+add_dotfile ~/dotfiles/.clang-format ~/.clang-format
+add_dotfile ~/dotfiles/.clang-tidy ~/.clang-tidy
+add_dotfile ~/dotfiles/.gitconfig ~/.gitconfig
+add_dotfile ~/dotfiles/.gitignore_global ~/.gitignore_global
+add_dotfile ~/dotfiles/.gitmessage.txt ~/.gitmessage.txt
+add_dotfile ~/dotfiles/.nanorc ~/.nanorc
+add_dotfile ~/dotfiles/.tmux.conf ~/.tmux.conf
+add_dotfile ~/dotfiles/vscode_settings.json ~/.config/Code/User/settings.json
