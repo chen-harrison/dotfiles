0a9cc2aa616b716162892f13a258bf049b420e09
Harrison Chen <hchen.robotics@gmail.com>
2026-02-26 12:51:45 -0500

    Source fzf-git.sh if available

--- .fzf.bash
+++ .fzf.bash
@@ -4,4 +4,8 @@ if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
+if [[ -f "${HOME}/.fzf/fzf-git.sh" ]] ; then
+  source "${HOME}/.fzf/fzf-git.sh"
+fi
+
 eval "$(fzf --bash)"
