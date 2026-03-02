63b849c55942f86541850777c2d1b0f49797f44d
Harrison Chen <hchen.robotics@gmail.com>
2026-03-02 18:14:12 -0500

    Improve Installation Script

--- install.sh
+++ install.sh
@@ -6,25 +6,35 @@
 add_dotfile () {
     local dotfile_path=$1
     local symlink_path=$2
-    local file_path
 
-    echo "Symlinking $dotfile_path to $symlink_path"
+    local symlink_dir
+    symlink_dir=$(dirname "$symlink_path")
+    if [ ! -d "$symlink_dir" ] ; then
+        echo "$symlink_dir does not exist - creating now"
+        mkdir -p "$symlink_dir" || sudo mkdir -p "$symlink_dir"
+    fi
 
-    # Get the file (or underlying file, for symlinks) at the symlink path
-    if [ -f "$symlink_path" ]; then
-        if [ -h "$symlink_path" ]; then
+    # Check if something exists at symlink_path
+    if [ -f "$symlink_path" ] ; then
+        local file_path
+        # Find the source file (if it's a symlink), otherwise grab the symlink_path
+        if [ -h "$symlink_path" ] ; then
             file_path=$(readlink -f "$symlink_path")
         else
             file_path=$symlink_path
         fi
 
-        # If the file is different from the source dotfile, store it in prev_dotfiles
+        # If the file is different from the source dotfile, copy it into prev_dotfiles
         if ! cmp -s "$dotfile_path" "$file_path" ; then
             echo "Saving $file_path to prev_dotfiles"
-            mv "$file_path" "${PWD}/prev_dotfiles/"
+            cp "$file_path" "${PWD}/prev_dotfiles/$(basename "$dotfile_path")"
+        else
+            echo -e "\e[0;33mSymlink to $dotfile_path already exists - skipping\e[0m"
+            return
         fi
     fi
 
+    echo -e "\e[0;32mSymlinking $dotfile_path to $symlink_path\e[0m"
     ln -sf "$dotfile_path" "$symlink_path"
 }
 
@@ -32,20 +42,23 @@ script_dir=$(dirname "$0")
 cd "$script_dir"
 mkdir -p "${PWD}/prev_dotfiles"
 
-add_dotfile ~/dotfiles/.bash_aliases ~/.bash_aliases
-add_dotfile ~/dotfiles/.bash_functions ~/.bash_functions
-add_dotfile ~/dotfiles/.bashrc ~/.bashrc
-add_dotfile ~/dotfiles/.clangd ~/.clangd
-add_dotfile ~/dotfiles/.clang-format ~/.clang-format
-add_dotfile ~/dotfiles/.clang-tidy ~/.clang-tidy
-add_dotfile ~/dotfiles/.fzf.bash ~/.fzf.bash
-add_dotfile ~/dotfiles/.gitconfig ~/.gitconfig
-add_dotfile ~/dotfiles/.gitignore_global ~/.gitignore_global
-add_dotfile ~/dotfiles/.gitmessage.txt ~/.gitmessage.txt
-add_dotfile ~/dotfiles/.nanorc ~/.nanorc
-add_dotfile ~/dotfiles/.shellcheckrc ~/.shellcheckrc
-add_dotfile ~/dotfiles/.tmux.conf ~/.tmux.conf
-add_dotfile ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
-add_dotfile ~/dotfiles/vscode/tasks.json ~/.config/Code/User/tasks.json
-add_dotfile ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
-add_dotfile ~/dotfiles/vscode/snippets.code-snippets ~/.config/Code/User/snippets/snippets.code-snippets
+# Find path to dotfiles directory, and grab files from there
+dotfiles_dir=$(find ~ -type d -name "dotfiles" 2> /dev/null | head -n 1)
+
+add_dotfile "$dotfiles_dir"/.bash_aliases ~/.bash_aliases
+add_dotfile "$dotfiles_dir"/.bash_functions ~/.bash_functions
+add_dotfile "$dotfiles_dir"/.bashrc ~/.bashrc
+add_dotfile "$dotfiles_dir"/.clangd ~/.clangd
+add_dotfile "$dotfiles_dir"/.clang-format ~/.clang-format
+add_dotfile "$dotfiles_dir"/.clang-tidy ~/.clang-tidy
+add_dotfile "$dotfiles_dir"/.fzf.bash ~/.fzf.bash
+add_dotfile "$dotfiles_dir"/.gitconfig ~/.gitconfig
+add_dotfile "$dotfiles_dir"/.gitignore_global ~/.gitignore_global
+add_dotfile "$dotfiles_dir"/.gitmessage.txt ~/.gitmessage.txt
+add_dotfile "$dotfiles_dir"/.nanorc ~/.nanorc
+add_dotfile "$dotfiles_dir"/.shellcheckrc ~/.shellcheckrc
+add_dotfile "$dotfiles_dir"/.tmux.conf ~/.tmux.conf
+add_dotfile "$dotfiles_dir"/vscode/settings.json ~/.config/Code/User/settings.json
+add_dotfile "$dotfiles_dir"/vscode/tasks.json ~/.config/Code/User/tasks.json
+add_dotfile "$dotfiles_dir"/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+add_dotfile "$dotfiles_dir"/vscode/snippets.code-snippets ~/.config/Code/User/snippets/snippets.code-snippets
